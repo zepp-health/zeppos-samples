12ed3467537ee77695831ae8b09e446e2cdcf5ab
shiyafei <shiyafei@zepp.com>
2023-09-25 15:39:15 +0800

    fix |> shake error not launch success

--- application/1.0/todo-list/app.js
+++ application/1.0/todo-list/app.js
@@ -2,26 +2,27 @@ import './shared/device-polyfill'
 import { MessageBuilder } from './shared/message'
 
 const logger = DeviceRuntimeCore.HmLogger.getLogger('todo-list-app')
-const appDevicePort = 20
-const appSidePort = 0
-const appId = 20001
-const messageBuilder = new MessageBuilder({
-  appId,
-  appDevicePort,
-  appSidePort
-})
 
 App({
   globalData: {
-    messageBuilder: messageBuilder
+    messageBuilder: null
   },
   onCreate() {
-    logger.log('app onCreate invoked')
-    messageBuilder.connect()
+    logger.log('app onCreate invoked');
+    let appId;
+    if (!hmApp.packageInfo) {
+      // appid = XXX // Modify appId
+      throw new Error('Set appId,  appId needs to be the same as the configuration in app.json');
+    } else {
+      appId = hmApp.packageInfo().appId;
+    }
+    this.globalData.messageBuilder = new MessageBuilder({
+      appId,
+    });
+    this.globalData.messageBuilder.connect();
   },
-
   onDestroy() {
-    logger.log('app onDestroy invoked')
-    messageBuilder.disConnect()
+    logger.log('app onDestroy invoked');
+    this.globalData.messageBuilder.disConnect();
   }
 })
