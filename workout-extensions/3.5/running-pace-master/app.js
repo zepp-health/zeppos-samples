1738ca732d91a8f8913d3c6b06418254e4a7600e
Myoung XUE - Zepp Health <xuemuyang@zepp.com>
2024-04-26 13:04:48 +0800

    feat: add workout extension sample

---
+++ workout-extensions/3.5/running-pace-master/app.js
@@ -0,0 +1,17 @@
+App({
+  globalData: {},
+
+  onCreate(options) {},
+
+  onShow(options) {},
+
+  onHide(options) {},
+
+  onDestroy(options) {},
+
+  onError(error) {},
+
+  onPageNotFound(obj) {},
+
+  onUnhandledRejection(obj) {},
+});
